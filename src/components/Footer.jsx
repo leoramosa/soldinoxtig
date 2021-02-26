38beaf9366bdedb84c2d89663a0add1b53b8cdbd
Leonardo Ramos <leo.ramos.ari@gmail.com>
2021-02-25 23:59:00 -0500

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './styles/footer.css'
-import './styles/Footertwo.scss'
 import { Link } from "react-router-dom";
  const Footer = () => {
   return (
@@ -11,7 +10,7 @@ import { Link } from "react-router-dom";
       
        <div className="content_info">
          <div className="info">
-           <p className="title-info">Contáctame</p>
+           <p className="title-info">Contáctanos</p>
            <div className="">
             <p>Teléfono: +51 981 325 313</p>
             <p>Email: ventas@soldinoxtig.com</p>
@@ -19,17 +18,17 @@ import { Link } from "react-router-dom";
            </div>
          </div>
          <div className="redes">
-            <p className="title-info">Sígueme:</p>
+            <p className="title-info">Síguenos:</p>
             <div className="icon-redes">
               <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fab fa-facebook-square"></i></a>
-              <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a>
+              <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a>
 
             </div>
          </div>
          <div className="explorer">
            <p className="title-info">Destacados</p>
            <div className="link-footer">
-            <Link className="footerLink" to="/productos-fabricados"><p>Productos</p></Link> 
+           <Link className="footerLink" to="/productos-fabricados"><p>Productos Fabricados</p></Link> 
             <Link className="footerLink" to="/servicios"><p>Servicios</p></Link> 
              
            </div>
@@ -39,7 +38,12 @@ import { Link } from "react-router-dom";
          <p>© Copyright Soldinoxtig 2021 -Todos los derechos reservados</p>
          <p className="diseño">Diseñado y desarrollado por <a className="dweb" href="https://leoramos.netlify.app/" target="_blank" rel="noreferrer"> Leonardo Ramos</a></p>
        </div>
-
+       <div className="derechos mobile">
+         <p>© Copyright Soldinoxtig 2021
+           <br/>
+           Todos los derechos reservados</p>
+         <p className="diseño">Diseñado y desarrollado por <a className="dweb" href="https://leoramos.netlify.app/" target="_blank" rel="noreferrer"> Leonardo Ramos</a></p>
+       </div>
        </div>
      </div>
     </>
