0526fc4f5cc99f33f222cb03b95e2fdc9520d8e2
Leonardo Ramos <leo.ramos.ari@gmail.com>
2021-03-03 20:20:36 -0500

    menssegner

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import NavBar from './../components/NavBar';
 import Footer from './../components/Footer';
 import './styles/layout.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat'
+
 
 const Layout = ( {children} ) => {
   return (
     <div className="layout-global">
       <NavBar/>
+      <MessengerCustomerChat
+    pageId="103633988458973"
+    appId="775872919759099"
+    /* htmlRef="<REF_STRING>" */
+  />
       <div className="layout-content">
         {children}
       </div>
